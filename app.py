2032e4e9df339f419fca2ffec4e9b4600904f339
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-04-20 23:18:29 -0400

    v1.2

--- app.py
+++ app.py
@@ -1,73 +1,127 @@
-import time
 from datetime import datetime
 from airflow.models.dag import DAG
 from airflow.decorators import task
 from airflow.utils.task_group import TaskGroup
-import pandas as pd
 import urllib.request
-import zipfile
-from sklearn.preprocessing import MinMaxScaler
 import pickle
+
+from pyspark.ml.feature import VectorAssembler, StandardScaler
+from pyspark.ml.regression import RandomForestRegressor
+from pyspark.sql.functions import col
+from pyspark.sql import SparkSession
 
 #Extract tasks
 @task()
 def download_data():
-    url = "http://files.grouplens.org/datasets/movielens/ml-100k.zip"
-    filename = "movielens.zip"
-    urllib.request.urlretrieve(url, filename)
-    with zipfile.ZipFile(filename, 'r') as zip_ref:
-        zip_ref.extractall()
+    url = "http://files.grouplens.org/datasets/movielens/ml-100k/u.data"
+    urllib.request.urlretrieve(url, "u.data")
     
 
 
 @task()
 def get_data():
-    df = pd.read_csv("ml-100k/u.data", sep="\t", header=None, names=["user_id", "item_id", "rating", "timestamp"])
-    with open('/data/file.pickle', 'wb') as f:
-        pickle.dump(df, f)
+    
+    #Spark session creation
+    spark = SparkSession.builder.appName("Recommendation_App").getOrCreate()
+
+    data = spark.read.format("csv").option("sep", "\t").option("header", False).load("u.data")
+    data = data.select(col("_c0").alias("userId"), col("_c1").alias("itemId"), col("_c2").alias("rating"), col("_c3").alias("timestamp"))
+    spark.stop()
+    return data
 
 #Transformation tasks
 @task()
-def missing_val():
-    with open('/path/to/shared/file.pickle', 'rb') as f:
-        df = pickle.load(f)
-    df.fillna(df.mean(), inplace=True)
-    return df
+def split_data(data):
+    (trainingData, testData) = data.randomSplit([0.8, 0.2])
+    return [trainingData, testData]
 
 @task()
-def normalization(df):
-    scaler = MinMaxScaler()
-    df[["rating"]] = scaler.fit_transform(train[["rating"]])
-    with open('/data/file.pickle', 'wb') as f:
-        pickle.dump(df, f)
+def missing_val(input_list):
+    trainingData=input_list[0]
+    testData= input_list[1]
+
+    trainingData = trainingData.fillna(trainingData.mean())
+    testData = testData.fillna(testData.mean())
+    return [trainingData, testData]
+
+@task()
+def normalization(input_list):
+    trainingData=input_list[0]
+    testData= input_list[1]
+
+    spark = SparkSession.builder.appName("Recommendation_App").getOrCreate()
+
+    assembler = VectorAssembler(inputCols=["userId", "itemId", "timestamp"], outputCol="features")
+    trainingData = assembler.transform(trainingData)
+    testData = assembler.transform(testData)
+
+    scaler = StandardScaler(inputCol="features", outputCol="scaledFeatures")
+    scalerModel = scaler.fit(trainingData)
+    trainingData = scalerModel.transform(trainingData)
+    testData = scalerModel.transform(testData)
     
+    spark.stop()
+    return [trainingData,testData] 
+
 
 #Load
 @task()
-def data_load():
-    with open('/path/to/shared/file.pickle', 'rb') as f:
-        df = pickle.load(f)
-    pass
+def data_load(input_list):
+    trainingData=input_list[0]
+    testData= input_list[1]
+
+    with open('/data/train_data.pickle', 'wb') as f:
+        pickle.dump(trainingData, f)
+
+    with open('/data/test_data.pickle', 'wb') as f:
+        pickle.dump(testData, f)
 
 
 #Model definer
 @task()
-def model_def():
-    pass
+def init_model():
+    spark = SparkSession.builder.appName("Recommendation_App").getOrCreate()
+
+    model = RandomForestRegressor(labelCol="rating", featuresCol="scaledFeatures", numTrees=10)
+    with open('/data/untrained_model.pickle', 'wb') as f:
+        pickle.dump(model, f)
+    
+    spark.stop()
+    
 
 #Train
 @task()
 def train():
-    pass
+    with open('/data/train_data.pickle', 'rb') as f:
+       trainingData = pickle.load(f)
+    with open('/data/untrained_model.pickle', 'rb') as f:
+       untrained_model = pickle.load(f)
+    model = untrained_model.fit(trainingData)
+    
+    
+    with open('/data/model.pickle', 'wb') as f:
+        pickle.dump(model, f)
+
+    
 
 #Test
 @task()
 def test():
-    pass
+    with open('/data/test_data.pickle', 'rb') as f:
+       testData = pickle.load(f)
+    with open('/data/model.pickle', 'rb') as f:
+       model = pickle.load(f)
+    
+    predictions = model.transform(testData)
+    
+    return predictions
 
 @task()
-def show_results():
-    pass
+def show_results(predictions):
+    predictions.show(10)
+    
+
+
 
 
 
@@ -75,27 +129,21 @@ def show_results():
 
 with DAG(dag_id="product_etl_dag",schedule_interval="0 9 * * *", start_date=datetime(2022, 3, 5),catchup=False,  tags=["product_model"]) as dag:
 
-    with TaskGroup("Extract", tooltip="Extract from data source") as extract:
+    with TaskGroup("ETL", tooltip="Extract Transform Load Data") as etl:
         dl_data = download_data()
         received_data = get_data()
+
+        spt_data = split_data(received_data)
+        miss_handle_data = missing_val(input_list = spt_data)
+        norm_data = normalization(input_list = miss_handle_data)
         
-        dl_data >> received_data
+        loaded_data = data_load(norm_data)
+
+        [dl_data, received_data, miss_handle_data, norm_data ,loaded_data] 
 
 
-    with TaskGroup("transform", tooltip="Transform data") as transform:
-        
-        miss_val = missing_val()
-        norm_val = normalization(miss_val)
-        
-        [miss_val, norm_val]
-
-    with TaskGroup("load", tooltip="Load to model") as load:
-        loaded_data = data_load()
-        
-        loaded_data
-
-    with TaskGroup("Model Definer", tooltip="Model Preparation") as model_definition:
-        model = model_def()
+    with TaskGroup("Model_Definer", tooltip="Model Preparation") as model_definition:
+        model = init_model()
         
         model
 
@@ -105,8 +153,8 @@ with DAG(dag_id="product_etl_dag",schedule_interval="0 9 * * *", start_date=date
         trained_model
 
     with TaskGroup("Test", tooltip="Testing phase") as testing:
-        test_model = test()
-        results = show_results()
-        test_model >> results
+        test_pred = test()
+        results = show_results(test_pred)
+        test_pred >> results
 
-    extract >> transform >> load >> model_definition >> training >> testing
+    etl >> model_definition >> training >> testing
